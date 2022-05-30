91735d27c6e120ad60ae7e88fb9a3f21ce6f3da5
Alexander <a.pride.xxi@tutanota.com>
2022-05-30 09:49:47 +0300

    initial commit

---
+++ Movie Box/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie Box
+//
+//  Created by Александр Прайд on 30.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
